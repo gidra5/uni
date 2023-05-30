50193800dae614bb19e59de865ad998078b19b97
gidra5 <hranitel.52@gmail.com>
2023-05-30 13:44:37 +0300

    upd

--- ts/src/parser/new.ts
+++ ts/src/parser/new.ts
@@ -1,10 +1,8 @@
 import { Iterator } from "../utils.js";
 
-type Token = string;
-
 export type ScopeGenerator = (enclosing: Scope) => Scope;
 export type SeparatorDefinition = {
-  tokens: Token[];
+  tokens: string[];
   repeats: [min: number, max: number];
   scope?: ScopeGenerator;
 };
@@ -12,9 +10,15 @@ export type OperatorDefinition = {
   separators: SeparatorDefinition[];
 };
 export type Scope = Record<string, OperatorDefinition>;
-export type OperatorInstance = { token: string; children: SeparatorInstance[] };
+
+export type Token =
+  | { type: "identifier" | "whitespace" | "newline"; src: string }
+  | { type: "number"; src: string; value: number }
+  | { type: "string"; src: string; value: string };
+export type OperatorInstance = { token: Token; children: SeparatorInstance[] };
+export type SeparatorChildren = (({ id: string } & OperatorInstance) | Token)[];
 export type SeparatorInstance = {
-  children: (({ id: string } & OperatorInstance) | Token)[];
+  children: SeparatorChildren;
   separatorIndex: number;
   separatorToken: Token;
 };
@@ -211,4 +215,172 @@ export const parseOperator = (
   }
 
   return [index, { token, children }, errors];
+};
+
+/* 
+Tokenizer algorithm:
+
+Input: 
+1. source string to be parsed
+2. starting index
+
+
+Output:
+1. Final index after parsing "token"
+2. parsed "token"
+3. List of errors that occured during parsing
+
+Instructions: 
+1.
+*/
+
+export const parseToken = (src: string, i: number): ParsingResult<Token> => {
+  let index = i;
+  const errors: ParsingError[] = [];
+
+  if (src.charAt(index) === '"') {
+    const start = index;
+    index++;
+
+    let value = "";
+    while (src.charAt(index) !== '"') {
+      if (src.charAt(index) === "\\") index++;
+      if (!src.charAt(index)) {
+        errors.push({ message: "end of source" });
+        break;
+      }
+      value += src.charAt(index);
+      index++;
+    }
+
+    return [
+      index,
+      { type: "string", src: src.substring(start, index), value },
+      errors,
+    ];
+  }
+
+  if (/\d/.test(src.charAt(index))) {
+    const start = index;
+
+    let value = "";
+    while (/[_\d]/.test(src.charAt(index))) {
+      if (src.charAt(index) === "_" && /\d/.test(src.charAt(index + 1))) {
+        index++;
+      } else if (src.charAt(index) === "_") break;
+      value += src.charAt(index);
+      index++;
+    }
+    if (src.charAt(index) === "." && /\d/.test(src.charAt(index + 1))) {
+      value += src.charAt(index);
+      index++;
+      while (/[_\d]/.test(src.charAt(index))) {
+        if (src.charAt(index) === "_" && /\d/.test(src.charAt(index + 1))) {
+          index++;
+        } else if (src.charAt(index) === "_") break;
+        value += src.charAt(index);
+        index++;
+      }
+    }
+
+    return [
+      index,
+      {
+        type: "number",
+        src: src.substring(start, index),
+        value: Number(value),
+      },
+      errors,
+    ];
+  }
+
+  if (/\s/.test(src.charAt(index))) {
+    const start = index;
+    while (/\s/.test(src.charAt(index))) index++;
+    const _src = src.substring(start, index);
+
+    return [
+      index,
+      { type: _src.includes("\n") ? "newline" : "whitespace", src: _src },
+      errors,
+    ];
+  }
+
+  if (/_\w/.test(src.charAt(index))) {
+    const start = index;
+    while (/[_\w\d]/.test(src.charAt(index))) index++;
+
+    return [
+      index,
+      { type: "identifier", src: src.substring(start, index) },
+      errors,
+    ];
+  }
+
+  if (/[^_\w\s\d"]/.test(src.charAt(index))) {
+    const start = index;
+    while (/[^_\w\s\d"]/.test(src.charAt(index))) index++;
+
+    return [
+      index,
+      { type: "identifier", src: src.substring(start, index) },
+      errors,
+    ];
+  }
+
+  errors.push({ message: "unexpected character" });
+  return [index, { type: "whitespace", src: "" }, errors];
+};
+
+/* 
+Pratt's Parsing algorithm:
+
+Input: 
+1. source tokenized operator instance to be parsed
+2. starting index in top children of operator instance
+3. min "precedence" of operators to be parsed.
+4. scope of "operator", that is being parsed - dictionary of possible "operators", which includes:
+  1. sequence of "separators", which include:
+    1. list of separator's definition tokens
+    2. number of repeats allowed for that separator
+    3. optional "scope" generator, that accepts enclosing scope 
+       and returns new scope to use while parsing its children, identity by default. Only used if min repeats > 0
+
+
+Output:
+1. Final index after parsing "node"
+2. parsed "node", that is either a "prefix", "postfix", "infix", or "atom" node
+3. List of errors that occured during parsing
+
+Instructions: 
+1.
+*/
+const prattParser = (
+  src: string,
+  i: number,
+  precedence: number,
+  scope: Scope
+) => {
+  // ExprNode left = switch (it.peek().type()) {
+  //   case Identifier -> {
+  //     var tokenNode = it.next();
+  //     yield switch (it.peek().type()) {
+  //       case Sub, Add, Mult, Div, Pow, Mod, Factorial, RParenthesis, EOT -> tokenNode;
+  //       default -> new FnCallExprNode(tokenNode, ExprNode.parse(it, 6, nodes));
+  //     };
+  //   }
+  //   case Number -> it.next();
+  //   case Sub -> new UnaryPrefixExprNode(it.next(), ExprNode.parse(it, 4, nodes));
+  //   case LParenthesis -> ExprNode.parseInParenthesis(it, nodes);
+  //   case Derivative -> DerivativeExprNode.parse(it, nodes);
+  //   default -> throw new FailedToParseException("Not expr");
+  // };
+  // while (precedence < it.peek().precedence()) {
+  //   left = switch (it.peek().type()) {
+  //     case Sub, Add, Mult, Div, Pow, Mod -> BinaryInfixExprNode.parse(it, left, nodes);
+  //     case Factorial -> new UnaryPostfixExprNode(left, it.next());
+  //     default -> left;
+  //   };
+  // }
+  // return left;
 };
