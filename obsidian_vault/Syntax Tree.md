dadb04b4c9719423e3ab8d9e1b9e4248f648f3a0
gidra5 <hranitel.52@gmail.com>
2024-01-15 15:36:42 +0200

    upd

--- obsidian_vault/Syntax Tree.md
+++ obsidian_vault/Syntax Tree.md
@@ -4,3 +4,16 @@ First are parsed all declarations in a module, then interpret declarations to ad
 
 Same strategy applied recursively after one layer is fully processed.
 
+While parsing expressions some nodes may declare new variables. For example
+* `1+2 is x and x > 1` -> `((1+2) is x) and (x > 1)` -> `(1+2) is x` declared `x` for the rest of expression and returned `boolean`, that will short-circuit when used with `and`
+* `if 1+2 is x: x+1 else 2` -> `if ((1+2) is x): (x+1) else 2` -> condition declared `x` for the `then` branch only
+* `{ x = 1; 2 + x }` -> `x = 1` declared `x` for the rest of the block
+* `for x, y of z: x + y` -> `x, y` declared `x` and `y` for the body of `for` loop
+* `while 1+2 is x: x+1` -> `1+2 is x` declared `x` for the body of `while` loop
+* `(1+2 is x and x+1) or (3-4 is y and y+2)` - `x` declared only for `x+1`
+* `(1+2 is x or 3+4 is x) and x-1` - `x` declared for `x-1`
+* `1+2 is x and x+1 is x and x-1` - `1+2 is x` declared `x`, `x+1 is x` redeclared `x` using prev declaration, and used in `x-1`
+* `x is of_type number and x+1` - `x is of_type number` declares type of `x` as `number` for the rest of expression and returned `boolean`, that will short-circuit when used with `and`
+
+`or` declares intersection of declarations in its operands for the rest of expression
+`and` declares union of declarations in its operands for the rest of expression
--- ts/src/parser/ast.ts
+++ ts/src/parser/ast.ts
@@ -1,9 +1,10 @@
 import { endOfTokensError, error } from "../errors";
 import Iterator from "../iterator";
+import { indexPosition, position } from "../position";
 import { parseTokens } from "../tokens";
 import { DefaultVisitor, Tree, Visitor } from "../tree";
 import { isEqual } from "../utils";
-import { ParsingError, TokenParser, TokenPos } from "./types";
+import { ParsingError, TokenParser } from "./types";
 
 export type Precedence = [prefix: number | null, postfix: number | null];
 const getPrecedence = (node: AbstractSyntaxTree, scope: Scope): Precedence =>
@@ -14,13 +15,6 @@ export type TokenGroupDefinition = {
   precedence: Precedence;
 };
 export type Scope = Record<string, TokenGroupDefinition>;
-
-const splitScope = (scope: Scope) => {
-  const [postfix, prefix] = Iterator.iterEntries(scope).partition(
-    ([_, { precedence }]) => precedence[0] !== null
-  );
-  return [postfix.toObject(), prefix.toObject()];
-};
 
 export type ParsingContext = {
   scope: Scope;
@@ -34,30 +28,108 @@ export const defaultParsingContext = (): ParsingContext => ({
 
 export type AbstractSyntaxTree = Tree & { value?: string };
 
-const placeholder = (): AbstractSyntaxTree => ({
+export const placeholder = (): AbstractSyntaxTree => ({
   name: "placeholder",
   children: [],
 });
-const name = (value: string): AbstractSyntaxTree => ({
+export const name = (value: string): AbstractSyntaxTree => ({
   name: "name",
   value,
   children: [],
 });
+export const group = (
+  value?: string,
+  children: AbstractSyntaxTree[] = []
+): AbstractSyntaxTree => ({
+  name: "group",
+  value,
+  children,
+});
+export const infix = (
+  ...children: [
+    group: AbstractSyntaxTree,
+    lhs: AbstractSyntaxTree,
+    rhs: AbstractSyntaxTree
+  ]
+): AbstractSyntaxTree => ({
+  name: "infix",
+  children,
+});
+export const postfix = (
+  ...children: [group: AbstractSyntaxTree, lhs: AbstractSyntaxTree]
+): AbstractSyntaxTree => ({
+  name: "postfix",
+  children,
+});
+export const prefix = (
+  ...children: [group: AbstractSyntaxTree, rhs: AbstractSyntaxTree]
+): AbstractSyntaxTree => ({
+  name: "prefix",
+  children,
+});
 
 export const parseGroup =
-  (context: ParsingContext): TokenParser<AbstractSyntaxTree> =>
+  (
+    context: ParsingContext,
+    scope = context.scope
+  ): TokenParser<AbstractSyntaxTree> =>
   (src, i = 0) => {
+    let index = i;
     const errors: ParsingError[] = [];
-    const matchingScope = Iterator.iterEntries(context.scope).filter(
-      ([_, { separators }]) => src[i].src === separators[0]
-    );
+    const matchingScope = Iterator.iterEntries(scope)
+      .filter(([_, { separators }]) => src[index].src === separators[0])
+      .filter(([_, { precedence }]) => {
+        return precedence[0] === null || precedence[0] >= context.precedence;
+      })
+      .filter(([_, { precedence }]) => {
+        if (context.lhs) return precedence[0] !== null;
+        return precedence[0] === null;
+      });
 
     if (matchingScope.isEmpty()) {
-      errors.push(error("Unexpected token", (src[i] as TokenPos).pos));
-      return [i, name(src[i].src), errors];
+      errors.push(error("Unexpected token", indexPosition(index)));
+      return [index + 1, group(), errors];
     }
 
-    return [i, placeholder(), errors];
+    const matchingScopeIsSingleSep = matchingScope
+      .map(([_, { separators }]) => separators.length === 1)
+      .every();
+
+    if (matchingScopeIsSingleSep) {
+      if (!matchingScope.skip(1).isEmpty()) {
+        errors.push(error("Ambiguous name", indexPosition(index)));
+      }
+
+      return [index + 1, name(src[index].src), errors];
+    }
+
+    index++;
+    if (src[index].type === "newline") index++;
+
+    const [nextIndex, expr, _errors] = parseExpr(context)(src, index);
+    if (_errors.length > 0) {
+      errors.push(
+        error("Errors in operand", position(index, nextIndex), _errors)
+      );
+    }
+    index = nextIndex;
+    if (src[index].type === "newline") index++;
+
+    const _scope = matchingScope
+      .spreadMap((k, v) => {
+        const entry = [k, { ...v, separators: v.separators.slice(1) }];
+        return entry as [key: string, value: TokenGroupDefinition];
+      })
+      .toObject();
+
+    const [nextIndex2, rest, _errors2] = parseGroup(context, _scope)(
+      src,
+      index
+    );
+    errors.push(..._errors2);
+    index = nextIndex2;
+
+    return [index, { ...rest, children: [expr, ...rest.children] }, errors];
   };
 
 export const parsePrefix =
@@ -77,10 +149,12 @@ export const parsePrefix =
     const [, right] = getPrecedence(group, context.scope);
 
     if (right !== null) {
-      const [nextIndex, rhs, _errors] = parseExpr(context)(src, index);
-      index = nextIndex;
+      const _context = { ...context, precedence: right };
+      let _errors: ParsingError[];
+      let rhs: AbstractSyntaxTree;
+      [index, rhs, _errors] = parseExpr(_context)(src, index);
       errors.push(..._errors);
-      return [index, { name: "prefix", children: [group, rhs] }, errors];
+      return [index, prefix(group, rhs), errors];
     }
 
     return [index, group, errors];
@@ -91,8 +165,9 @@ export const parseExpr =
   (src, i = 0) => {
     let index = i;
     const errors: ParsingError[] = [];
-    let [nextIndex, lhs, _errors] = parsePrefix(context)(src, index);
-    index = nextIndex;
+    let _errors: ParsingError[];
+    context.lhs = undefined;
+    [index, context.lhs, _errors] = parsePrefix(context)(src, index);
     errors.push(..._errors);
 
     while (src[index]) {
@@ -101,21 +176,27 @@ export const parseExpr =
         index++;
         continue;
       }
-      let [nextIndex, token, _errors] = parseGroup(context)(src, index);
+      let [nextIndex, group, _errors] = parseGroup(context)(src, index);
       errors.push(..._errors);
-      const [left, right] = getPrecedence(token, context.scope);
-      if (left === null || left < context.precedence) break;
+      if (!group.value) break;
+      const [, right] = getPrecedence(group, context.scope);
       index = nextIndex;
+
       if (right === null) {
-        lhs = { name: "postfix", children: [token, lhs] };
+        context.lhs = postfix(group, context.lhs);
+        break;
       } else {
-        const [nextIndex, rhs, _errors] = parseExpr(context)(src, index);
-        index = nextIndex;
+        const _context = { ...context, precedence: right };
+        let _errors: ParsingError[];
+        let rhs: AbstractSyntaxTree;
+        [index, rhs, _errors] = parseExpr(_context)(src, index);
         errors.push(..._errors);
-        lhs = { name: "infix", children: [token, lhs, rhs] };
+        context.lhs = infix(group, context.lhs, rhs);
       }
     }
 
+    const lhs = context.lhs;
+    context.lhs = undefined;
     return [index, lhs, errors];
   };
 
