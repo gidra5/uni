e90a29d6a18ece7edf2e2f9fe34cb1e574dcd761
gidra5 <hranitel.52@gmail.com>
2024-03-09 17:09:34 +0200

    upd

--- ts/tests/evaluation.test.ts
+++ ts/tests/evaluation.test.ts
@@ -13,18 +13,18 @@ export const errorsTestCase = (src, expectedErrors, _it: any = it) =>
     expect(errors).toEqual(expectedErrors);
   });
 
-export const evalTestCase = (src, expectedTree?) => {
+export const evalTestCase = (src, expectedValue?) => {
   const [tokens] = parseTokens(src);
   const [tree, errors] = parse()(tokens);
   // console.dir(tree, { depth: null });
   expect(errors).toEqual([]);
   const _tree = omitASTDataScope(tree);
-  if (expectedTree) expect(_tree).toEqual(expectedTree);
+  if (expectedValue) expect(_tree).toEqual(expectedValue);
   expect(_tree).toMatchSnapshot();
 };
 
-export const evalTestCaseArgs = (src, expectedTree?) =>
-  [`produces correct tree for '${src}'`, () => evalTestCase(src, expectedTree)] as const;
+export const evalTestCaseArgs = (src, expectedValue?) =>
+  [`produces correct tree for '${src}'`, () => evalTestCase(src, expectedValue)] as const;
 
 /* one test per example of a language construct  */
 
@@ -89,7 +89,7 @@ describe("expressions", () => {
   describe("boolean expressions", () => {
     test("not", () => {
       const src = `!123`;
-      evalTestCase(src, null, pick(scopeDictionary, ["!"]));
+      evalTestCase(src);
     });
 
     describe("comparators", () => {
@@ -101,14 +101,14 @@ describe("expressions", () => {
       for (const op of Iterator.iter(comparators).flat()) {
         test(`comparator ${op}`, () => {
           const src = `123 ${op} 456`;
-          evalTestCase(src, null, pick(scopeDictionary, [op]));
+          evalTestCase(src);
         });
       }
 
       for (const [op1, op2] of Iterator.iter(comparators).flatMap((pair) => Iterator.iter(pair).power(2))) {
         test(`range ${op1} ${op2}`, () => {
           const src = `123 ${op1} x ${op2} 456`;
-          evalTestCase(src, null, pick(scopeDictionary, [`inRange_${op1}_${op2}`]));
+          evalTestCase(src);
         });
       }
     });
@@ -117,28 +117,28 @@ describe("expressions", () => {
   describe("function expressions", () => {
     test("function with no arg", () => {
       const src = `fn -> #0`;
-      evalTestCase(src, null, pick(scopeDictionary, ["fn", "#"]));
+      evalTestCase(src);
     });
 
     test("function with shadowed name access", () => {
       const src = `fn a -> fn a -> #a`;
-      evalTestCase(src, null, pick(scopeDictionary, ["fn", "#"]));
+      evalTestCase(src);
     });
 
     test("function with deep shadowed name access", () => {
       const src = `fn a -> fn a -> fn a -> ##a`;
-      evalTestCase(src, null, pick(scopeDictionary, ["fn", "#"]));
+      evalTestCase(src);
     });
 
     describe("application", () => {
       test("function call", () => {
         const src = `f x`;
-        evalTestCase(src, null, pick(scopeDictionary, ["application"]));
+        evalTestCase(src);
       });
 
       test("function call multiple args", () => {
         const src = `f x y`;
-        evalTestCase(src, null, pick(scopeDictionary, ["application"]));
+        evalTestCase(src);
       });
     });
   });
@@ -372,8 +372,6 @@ describe("expressions", () => {
   });
 
   describe("data structures", () => {
-    it(...evalTestCaseArgs("(-(2+7)/A,j, i, 127.0 )"));
-
     test("unit", () => {
       const src = `()`;
       evalTestCase(src);
@@ -615,254 +613,5 @@ describe("expressions", () => {
       const src = `signal (x + y)`;
       evalTestCase(src);
     });
-  });
-});
-
-describe("programs", () => {
-  describe("script", () => {
-    test("use", () => {
-      const src = `use "a" as b`;
-      evalTestCase(src);
-    });
-    test("use with", () => {
-      const src = `use "a" as b with x`;
-      evalTestCase(src);
-    });
-    test("export", () => {
-      const src = `export x`;
-      evalTestCase(src);
-    });
-    test("export as", () => {
-      const src = `export x as y`;
-      evalTestCase(src);
-    });
-
-    test("variable use", () => {
-      const src = `x := 123;  use "a" as b`;
-      evalTestCase(src);
-    });
-    test("variable use with", () => {
-      const src = `x := 123;  use "a" as b with x`;
-      evalTestCase(src);
-    });
-    test("variable export", () => {
-      const src = `x := 123; export x`;
-      evalTestCase(src);
-    });
-    test("variable export as", () => {
-      const src = `x := 123; export x as y`;
-      evalTestCase(src);
-    });
-  });
-
-  describe("module", () => {
-    test("import", () => {
-      const src = `import "a" as b`;
-      evalTestCase(src);
-    });
-    test("import with", () => {
-      const src = `import "a" as b with c`;
-      evalTestCase(src);
-    });
-    test("import with external", () => {
-      const src = `import "a" as b with external c`;
-      evalTestCase(src);
-    });
-    test("external", () => {
-      const src = `external y`;
-      evalTestCase(src);
-    });
-    test("private declare", () => {
-      const src = `z := y+1`;
-      evalTestCase(src);
-    });
-    test("public declare", () => {
-      const src = `export x := z+123`;
-      evalTestCase(src);
-    });
-    test("export main", () => {
-      const src = `export args -> {}`;
-      evalTestCase(src);
-    });
-    test("operator", () => {
-      const src = `operator _+_ := fn x, y -> x + y`;
-      evalTestCase(src);
-    });
-    test("operator with precedence", () => {
-      const src = `operator _+_ precedence 1 := fn x, y -> x + y`;
-      evalTestCase(src);
-    });
-    test("operator with tuple precedence", () => {
-      const src = `operator _+_ precedence 1, 2 := fn x, y -> x + y`;
-      evalTestCase(src);
-    });
-  });
-});
-
-describe("newline handling", () => {
-  test("block newline in the middle", () => {
-    const src = `{ a := 1\n b := 2 }`;
-    evalTestCase(src);
-  });
-
-  test("block newline at the end", () => {
-    const src = `{ a := 1\n b := 2\n }`;
-    evalTestCase(src);
-  });
-
-  test("block newline at the beginning", () => {
-    const src = `{\n a := 1\n b := 2 }`;
-    evalTestCase(src);
-  });
-
-  test("block semicolon newline", () => {
-    const src = `{ a := 1;\n b := 2 }`;
-    evalTestCase(src);
-  });
-
-  test("block semicolon newline at the end", () => {
-    const src = `{ a := 1;\n b := 2;\n }`;
-    evalTestCase(src);
-  });
-});
-
-describe("examples", () => {
-  describe("procedural", () => {
-    test("hello world", () => {
-      const src = `
-        external println
-        export args -> {
-          println "Hello, World!"
-        }`;
-      exampleTestCase(src);
-    });
-
-    test("fibonacci", () => {
-      const src = `
-        export fib := n -> {
-          if n < 2
-            1
-          else
-            fib (n - 1) + fib (n - 2)
-        }`;
-
-      exampleTestCase(src);
-    });
-
-    test.todo("quick sort recursive", () => {
-      const src = `
-        export quicksort := xs -> {
-          if xs == ()
-            return ()
-          
-          (pivot, ...rest) := xs
-          smaller := for x in rest: if x < pivot: x
-          bigger := for x in rest: if x >= pivot: x
-          return (...quicksort smaller, pivot, ...quicksort bigger)
-        }`;
-
-      exampleTestCase(src);
-    });
-
-    test.todo("bubble sort imperative", () => {
-      const src = `
-        export bubblesort := fn mut list -> {
-          for i in range 0 list.length
-          for j in range 0 (list.length - i - 1)
-          if list[j] > list[j+1]
-            list[j], list[j+1] = list[j+1], list[j]
-          return list
-        }`;
-
-      exampleTestCase(src);
-    });
-  });
-
-  describe.todo("functional", () => {
-    test("option", () => {
-      const src = `
-        export none := fn some, none -> none
-        export some := value -> fn some, none -> some value
-        export map := fn f, option -> match option {
-          none -> none
-          some value -> some (f value)
-        }
-        export unwrap := fn option, default -> match option {
-          none -> default
-          some value -> value
-        }
-        export flat_map := fn f, option -> match option {
-          none -> none
-          some value -> f value
-        }
-      `;
-      exampleTestCase(src);
-    });
-
-    test("result", () => {
-      const src = `
-        export ok := value -> fn ok, err -> ok value
-        export err := value -> fn ok, err -> err value
-        export map := fn f, result ->
-          if result is ok value: ok (f value) else: result
-        export unwrap := fn result, default ->
-          if result is ok value: value else: default
-        export flat_map := fn f, result ->
-          if result is ok value: f value else: result
-      `;
-      exampleTestCase(src);
-    });
-
-    test("tuple", () => {
-      const src = `
-        export tuple := fn x, y -> fn match -> match x y
-        export first := fn tuple -> tuple (fn x, y -> x)
-        export second := fn tuple -> tuple (fn x, y -> y)
-      `;
-      exampleTestCase(src);
-    });
-
-    test("list", () => {
-      const src = `
-        symbol EmptyList
-        export nil := fn cons, nil -> nil
-        export cons := fn x, xs -> fn cons, nil -> cons x xs
-        export head := fn list -> list (fn x, xs -> x) EmptyList
-        export tail := fn list -> list (fn x, xs -> xs) EmptyList
-        export map := fn f, list -> list (fn x, xs -> cons (f x) (map f xs)) nil
-      `;
-      exampleTestCase(src);
-    });
-
-    test.todo("map", () => {
-      const src = `
-        symbol NotFound
-        export empty := fn entry, empty -> empty
-        export entry := fn key, value, rest -> fn entry, empty -> entry key value rest
-        export find := fn key, map -> map
-          (fn _key, value, rest ->
-            if key == _key: value else find key rest
-          )
-          NotFound
-        export insert := fn key, value, map -> entry key value (remove key map)
-        export remove := fn key, map -> map
-          (fn _key, value, rest ->
-            if key == _key: rest else remove key rest
-          )
-          empty
-        operator _[_] = fn map, key -> find key map
-      `;
-      exampleTestCase(src);
-    });
-  });
-
-  test.todo("prototype", () => {
-    const src = `
-      symbol Prototype
-      operator _._ = fn value, name -> 
-        if value[name] is some value: value
-        else accessor value[Prototype] name
-    `;
-    exampleTestCase(src);
   });
 });
