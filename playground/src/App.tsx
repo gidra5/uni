43763e6b43affac801ce510b32227d5b405dde78
Roman Khranovskiy <hranitel.52@gmail.com>
2024-10-15 15:12:39 +0300

    vault backup: 2024-10-15 15:12:39

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,35 +1,29 @@
-import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createSignal } from "solid-js";
+import Editor from "solid-simple-code-editor";
+import { highlight, languages } from "prismjs/components/prism-core";
 
-function App() {
-  const [count, setCount] = createSignal(0)
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-jsx";
+
+const initial = `
+function add(a, b) {
+  return a + b;
+}
+`;
+
+export const App = () => {
+  const [code, setCode] = createSignal(initial);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <Editor
+      value={code()}
+      onValueChange={(code) => setCode(code)}
+      highlight={(code) => highlight(code, languages.jsx, "jsx")}
+      padding="10px"
+      style={{
+        "font-family": '"Fira code", "Fira Mono", monospace',
+        "font-size": "12px",
+      }}
+    />
+  );
+};
