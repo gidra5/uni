c076081f5989c973d9204379d178f09adeb91c9e
gidra5 <hranitel.52@gmail.com>
2024-12-09 13:47:18 +0200

    upd

--- ts/tests/tokenGroups.test.ts
+++ ts/tests/tokenGroups.test.ts
@@ -8,8 +8,6 @@ import {
 } from "../src/parser/tokenGroups.js";
 import { describe, expect } from "vitest";
 import { it, fc, test } from "@fast-check/vitest";
-// import { describe, expect, test } from "vitest";
-// import { fc } from "@fast-check/vitest";
 import { type Arbitrary } from "fast-check";
 import { Iterator } from "iterator-js";
 import { eventLoopYield } from "../src/utils/index.js";
@@ -32,48 +30,30 @@ function clearToken({ start, end, ...token }: any) {
     return {
       type: "error",
       cause: token.cause,
-      token: token.token.map(clearToken),
+      token: clearToken(token.token),
     };
   }
   return token;
 }
 
 describe("string interpolation", () => {
-  test.prop(
-    [
-      fc
-        .array(stringInsidesArb)
-        .chain((textSegments) =>
-          arrayLenArb(
-            anyStringArb.filter((s) => !s.includes("(") && !s.includes(")") && !s.includes('"')),
-            Math.max(textSegments.length - 1, 0)
-          ).map((joins) => [textSegments, joins])
-        )
-        .map(([strings, joins]) => {
-          const literal = Iterator.zip(strings, [...joins.map((x) => `\\(${x})`), ""])
-            .flat()
-            .join("");
-          const value = Iterator.zip(strings, [...joins, ""]);
-          return [literal, value.toArray()] as const;
-        }),
-    ],
-    {
-      // seed: -968732021,
-      // path: "2:2:1:5:8:10:12:13:13:0:0:0:1:3:2:3",
-      // endOnFailure: true,
-      examples: [
-        [
-          [
-            '\\(")',
-            [
-              ["", '"'],
-              ["", ""],
-            ],
-          ],
-        ],
-      ],
-    }
-  )("template strings", async ([literal, interpolated]) => {
+  test.prop([
+    fc
+      .array(stringInsidesArb)
+      .chain((textSegments) =>
+        arrayLenArb(
+          anyStringArb.filter((s) => !s.includes("(") && !s.includes(")") && !s.includes('"')),
+          Math.max(textSegments.length - 1, 0)
+        ).map((joins) => [textSegments, joins])
+      )
+      .map(([strings, joins]) => {
+        const literal = Iterator.zip(strings, [...joins.map((x) => `\\(${x})`), ""])
+          .flat()
+          .join("");
+        const value = Iterator.zip(strings, [...joins, ""]);
+        return [literal, value.toArray()] as const;
+      }),
+  ])("template strings", async ([literal, interpolated]) => {
     await eventLoopYield();
     const value: TokenGroup[] = interpolated
       .flatMap(([text, interpolated]): TokenGroup[] => [
@@ -88,12 +68,13 @@ describe("string interpolation", () => {
       ])
       .slice(0, -1);
     if (value.length === 0) value.push({ type: "string", value: "" } as TokenGroup);
+
     const src = `"${literal}"`;
     const startIndex = 0;
     const expectedIndex = src.length;
     const expectedToken = { type: "group", kind: TokenGroupKind.StringTemplate, tokens: value };
     const [{ index }, token] = _parseToken.parse(src, { index: startIndex, followSet: [] });
-    console.dir({ token: clearToken(token), expectedToken }, { depth: null });
+
     expect(index).toBe(expectedIndex);
     expect(clearToken(token)).toEqual(expectedToken);
   });
