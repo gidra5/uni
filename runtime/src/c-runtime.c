92319d4cfebfce3feafcaea9cf9fa7f73be99229
Roman Khranovskiy <hranitel.52@gmail.com>
2025-02-20 20:55:47 +0200

    upd

--- runtime/src/c-runtime.c
+++ runtime/src/c-runtime.c
@@ -1,16 +1,25 @@
+#include <assert.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 
+#define assert_msg(x, msg) \
+  printf(msg);             \
+  assert(x);
+
+#define unreachable(x) assert_msg(false, "\n\nUNREACHABLE: " x "\n")
+#define _assert(x, msg) assert_msg(false, "\n\nASSERTION FAILED: " x "\n")
+#define max(a, b) ((a) > (b) ? (a) : (b))
+
 enum type_t {
-  INT,
-  FLOAT,
-  STRING,
-  SYMBOL,
-  BOOLEAN,
-  TUPLE,
-  POINTER,
-  FUNCTION,
+  TYPE_INT = 1,
+  TYPE_FLOAT = 2,
+  TYPE_STRING = 3,
+  TYPE_SYMBOL = 4,
+  TYPE_BOOLEAN = 5,
+  TYPE_TUPLE = 6,
+  TYPE_POINTER = 7,
+  // TYPE_FUNCTION,
 };
 
 struct int_type_metadata_t {
@@ -27,22 +36,22 @@ struct string_type_metadata_t {
 
 struct tuple_type_metadata_t {
   int count;
-  enum type_t* types;
+  struct type_metadata_t* types;
 };
 
 struct pointer_type_metadata_t {
   struct type_metadata_t* type;
 };
 
-struct function_type_metadata_t {
-  struct type_metadata_t* ret;
+// struct function_type_metadata_t {
+//   struct type_metadata_t* ret;
 
-  int args_count;
-  struct type_metadata_t* args;
+//   int args_count;
+//   struct type_metadata_t* args;
 
-  int closure_count;
-  struct type_metadata_t* closure;
-};
+//   int closure_count;
+//   struct type_metadata_t* closure;
+// };
 
 struct type_metadata_t {
   enum type_t type;
@@ -52,45 +61,124 @@ struct type_metadata_t {
     struct string_type_metadata_t string;
     struct tuple_type_metadata_t tuple;
     struct pointer_type_metadata_t pointer;
-    struct function_type_metadata_t function;
+    // struct function_type_metadata_t function;
   } metadata;
 };
 
-struct type_metadata_t function_closure_type(
-  struct function_type_metadata_t type
-) {
-  struct type_metadata_t closure_type = {
-    .type = TUPLE,
-    .metadata.tuple.count = type.closure_count,
-    .metadata.tuple.types = type.closure
-  };
-  return closure_type;
+// struct type_metadata_t function_closure_type(
+//   struct function_type_metadata_t type
+// ) {
+//   struct type_metadata_t closure_type = {
+//     .type = TUPLE,
+//     .metadata.tuple.count = type.closure_count,
+//     .metadata.tuple.types = type.closure
+//   };
+//   return closure_type;
+// }
+
+int type_alignment(struct type_metadata_t type);
+int max_alignment(int count, struct type_metadata_t* types) {
+  int alignment = 0;
+  for (int i = 0; i < count; i++) {
+    alignment = max(alignment, type_alignment(types[i]));
+  }
+  return alignment;
+}
+
+int type_alignment(struct type_metadata_t type) {
+  switch (type.type) {
+    case TYPE_INT:
+      return type.metadata._int.size;
+    case TYPE_FLOAT:
+      return type.metadata._float.size;
+    case TYPE_STRING:
+      return 8;
+    case TYPE_BOOLEAN:
+      return 1;
+    case TYPE_TUPLE: {
+      return max_alignment(
+        type.metadata.tuple.count, type.metadata.tuple.types
+      );
+    }
+    case TYPE_POINTER:
+      return 8;
+    // case TYPE_FUNCTION:
+    //   struct type_metadata_t closure = {
+    //     .type = TUPLE,
+    //     .metadata.tuple.count = type.metadata.function.closure_count,
+    //     .metadata.tuple.types = type.metadata.function.closure
+    //   };
+    //   return type_size(closure) + 8;
+    default:
+      unreachable("cant compute type size");
+  }
 }
 
 int type_size(struct type_metadata_t type) {
   switch (type.type) {
-    case INT:
+    case TYPE_INT:
       return type.metadata._int.size;
-    case FLOAT:
+    case TYPE_FLOAT:
       return type.metadata._float.size;
-    case STRING:
+    case TYPE_STRING:
       return 8;
-    case BOOLEAN:
+    case TYPE_BOOLEAN:
       return 1;
-    case TUPLE:
-      return type.metadata.tuple.count;
-    case POINTER:
+    case TYPE_TUPLE: {
+      int size = 0;
+      int prev_alignment = type_alignment(type.metadata.tuple.types[0]);
+      for (int i = 0; i < type.metadata.tuple.count; i++) {
+        int alignment = type_alignment(type.metadata.tuple.types[i]);
+
+        if (alignment > prev_alignment) {
+          int padding = alignment - (size % alignment);
+          size += padding + type_size(type.metadata.tuple.types[i]);
+        } else {
+          size += type_size(type.metadata.tuple.types[i]);
+        }
+
+        prev_alignment = alignment;
+      }
+      return size;
+    }
+    case TYPE_POINTER:
       return 8;
-    case FUNCTION:
-      struct type_metadata_t closure = {
-        .type = TUPLE,
-        .metadata.tuple.count = type.metadata.function.closure_count,
-        .metadata.tuple.types = type.metadata.function.closure
-      };
-      return type_size(closure) + 8;
+    // case TYPE_FUNCTION:
+    //   struct type_metadata_t closure = {
+    //     .type = TUPLE,
+    //     .metadata.tuple.count = type.metadata.function.closure_count,
+    //     .metadata.tuple.types = type.metadata.function.closure
+    //   };
+    //   return type_size(closure) + 8;
     default:
       unreachable("cant compute type size");
   }
+}
+
+void* get_element_ptr(
+  void* x,
+  int index,
+  int count,
+  struct type_metadata_t* types
+) {
+  assert(index < count);
+
+  int size = 0;
+  int alignment = type_alignment(types[0]);
+  for (int i = 0; i < count; i++) {
+    if (i == index)
+      return (char*)x + size;
+
+    size += type_size(types[i]);
+
+    int next_alignment = type_alignment(types[i + 1]);
+    if (next_alignment > alignment)
+      size += next_alignment - (size % next_alignment);
+
+    alignment = next_alignment;
+  }
+
+  unreachable("cant get element ptr");
 }
 
 struct symbol_metadata_t {
@@ -100,47 +188,49 @@ struct symbol_metadata_t {
 extern struct symbol_metadata_t* symbols_metadata;
 
 void print_int(int x) {
-  printf("%i\n", x);
+  printf("%i", x);
 }
 
 void print_float(float x) {
-  printf("%f\n", x);
+  printf("%f", x);
 }
 
 void print_string(char* x) {
-  printf("%s\n", x);
+  printf("%s", x);
 }
 
 void print_symbol(uint64_t x) {
   struct symbol_metadata_t sym = symbols_metadata[x];
-  printf("Symbol(%s)\n", sym.name);
+  printf("Symbol(%s)", sym.name);
 }
 
 void print_bool(bool x) {
-  printf("%s\n", x ? "true" : "false");
+  printf("%s", x ? "true" : "false");
 }
 
-void print_function(void* x, struct function_type_metadata_t type) {
-  int closure_size = type_size(function_closure_type(type));
+// void print_function(void* x, struct function_type_metadata_t type) {
+//   int closure_size = type_size(function_closure_type(type));
 
-  void* fn = x;
-  x = (void*)((char*)x + 8);
+//   void* fn = x;
+//   x = (void*)((char*)x + 8);
 
-  printf("Function[");
+//   printf("Function[");
 
-  if (type.closure_count > 0) {
-    print_by_type(x, type.closure[0]);
-    x = (char*)x + type_size(type.closure[0]);
-  }
+//   if (type.closure_count > 0) {
+//     print_by_type(x, type.closure[0]);
+//     x = (char*)x + type_size(type.closure[0]);
+//   }
 
-  for (int i = 1; i < type.closure_count; i++) {
-    printf(", ");
-    print_by_type(x, type.closure[i]);
-    x = (char*)x + type_size(type.closure[i]);
-  }
+//   for (int i = 1; i < type.closure_count; i++) {
+//     printf(", ");
+//     print_by_type(x, type.closure[i]);
+//     x = (char*)x + type_size(type.closure[i]);
+//   }
 
-  printf("](%p)\n", *(void**)x);
-}
+//   printf("](%p)\n", *(void**)x);
+// }
+
+void print_by_type(void* x, struct type_metadata_t type);
 
 void print_tuple(void* x, int count, struct type_metadata_t* types) {
   printf("Tuple(");
@@ -148,39 +238,40 @@ void print_tuple(void* x, int count, struct type_metadata_t* types) {
   for (int i = 0; i < count; i++) {
     if (i > 0)
       printf(", ");
-    print_by_type(x, types[i]);
-    x = (char*)x + type_size(types[i]);
+
+    void* element_ptr = get_element_ptr(x, i, count, types);
+    print_by_type(element_ptr, types[i]);
   }
 
-  printf(")\n");
+  printf(")");
 }
 
 void print_by_type(void* x, struct type_metadata_t type) {
   switch (type.type) {
-    case INT:
+    case TYPE_INT:
       print_int(*(int*)x);
       break;
-    case FLOAT:
+    case TYPE_FLOAT:
       print_float(*(float*)x);
       break;
-    case STRING:
+    case TYPE_STRING:
       print_string(*(char**)x);
       break;
-    case BOOLEAN:
+    case TYPE_BOOLEAN:
       print_bool(*(bool*)x);
       break;
-    case SYMBOL:
+    case TYPE_SYMBOL:
       print_symbol(*(uint64_t*)x);
       break;
-    case TUPLE:
+    case TYPE_TUPLE:
       print_tuple(x, type.metadata.tuple.count, type.metadata.tuple.types);
       break;
-    case POINTER:
-      printf("Pointer(%p)\n", x);
+    case TYPE_POINTER:
+      printf("Pointer(%p)", x);
       break;
-    case FUNCTION:
-      print_function(x, type.metadata.function);
-      break;
+    // case TYPE_FUNCTION:
+    //   print_function(x, type.metadata.function);
+    //   break;
     default:
       unreachable("cant print by type");
   }
--- ts/test.ll
+++ ts/test.ll
@@ -1,50 +1,75 @@
-@.str = constant [4 x i8] c"%i\0A\00"
+@symbols_metadata_array = global [0 x { i8* }] []
+@symbols_metadata = global ptr @symbols_metadata_array
 
-%closure_type = type { i64 }
-%fn_type = type i64 (i64)*
-%closure_fn_type = type { i64 (%closure_type, %fn_type)*, %closure_type* }
-declare i32 @c_hello()
-declare i32 @cuda_hello()
+; enum type_t {
+;   TYPE_INT = 1,
+;   TYPE_FLOAT = 2,
+;   TYPE_STRING = 3,
+;   TYPE_SYMBOL = 4,
+;   TYPE_BOOLEAN = 5,
+;   TYPE_TUPLE = 6,
+;   TYPE_POINTER = 7,
+;   // TYPE_FUNCTION,
+; };
+
+%struct.type_metadata_t = type { i32, %union.anon }
+%union.anon = type { %struct.tuple_type_metadata_t }
+%struct.tuple_type_metadata_t = type { i32, ptr }
+%struct.int_type_metadata_t = type { i32 }
+%struct.int_type_metadata_t_padded = type { i32, [8 x i8] }
+%struct.float_type_metadata_t = type { i32 }
+%struct.float_type_metadata_t_padded = type { i32, [8 x i8] }
+%struct.string_type_metadata_t = type { i32 }
+%struct.string_type_metadata_t_padded = type { i32, [8 x i8] }
+%struct.symbol_metadata_t = type { ptr }
+
+%tuple_type = type { i32, i1, i1, ptr, i32, { i1, ptr } }
+@string = global [4 x i8] c"abc\00"
+@tuple = global %tuple_type { i32 1, i1 0, i1 1, ptr @string, i32 2, { i1, ptr } { i1 0, ptr @string } }
+; @tuple = global { i32, ptr } { i32 1, ptr @string }
+@newline = global [2 x i8] c"\0A\00"
+@pointer_fmt = global [4 x i8] c"%p\0A\00"
+
+declare void @print_tuple(ptr, i32, ptr)
+declare i32 @printf(ptr, ...)
 
 define i32 @main() {
 entry:
-  %closure_fn_ptr = alloca %closure_fn_type
-  call void @createClosure(ptr %closure_fn_ptr, i64 2)
-  %var_0 = load %closure_fn_type, ptr %closure_fn_ptr
-  %closure_value_ptr = extractvalue %closure_fn_type %var_0, 1
-  %closure_value = load %closure_type, ptr %closure_value_ptr
-  %closure_fn = extractvalue %closure_fn_type %var_0, 0
-  %var_1 = call i64 %closure_fn(%closure_type %closure_value, %fn_type @fn)
-  call void @printf([4 x i8]* @.str, i64 %var_1)
-  call void @c_hello()
-  call void @cuda_hello()
+  %b_ptr = alloca [2 x %struct.type_metadata_t]
+  %b1_ptr = getelementptr inbounds %struct.type_metadata_t, ptr %b_ptr, i64 0, i32 0
+  %b2_ptr = getelementptr inbounds %struct.type_metadata_t, ptr %b_ptr, i64 1, i32 0
+  %b2_metadata_ptr = getelementptr inbounds %struct.type_metadata_t, ptr %a_ptr, i64 1, i32 1, i32 0
+  store i32 5, ptr %b1_ptr
+  store i32 3, ptr %b2_ptr
+  store i32 4, ptr %b2_metadata_ptr
+
+  %a_ptr = alloca [6 x %struct.type_metadata_t]
+  %a1_ptr = getelementptr inbounds %struct.type_metadata_t, ptr %a_ptr, i64 0, i32 0
+  %a2_ptr = getelementptr inbounds %struct.type_metadata_t, ptr %a_ptr, i64 1, i32 0
+  %a3_ptr = getelementptr inbounds %struct.type_metadata_t, ptr %a_ptr, i64 2, i32 0
+  %a4_ptr = getelementptr inbounds %struct.type_metadata_t, ptr %a_ptr, i64 3, i32 0
+  %a5_ptr = getelementptr inbounds %struct.type_metadata_t, ptr %a_ptr, i64 4, i32 0
+  %a6_ptr = getelementptr inbounds %struct.type_metadata_t, ptr %a_ptr, i64 5, i32 0
+  %a1_metadata_ptr = getelementptr inbounds %struct.type_metadata_t, ptr %a_ptr, i64 0, i32 1, i32 0
+  %a3_metadata_ptr = getelementptr inbounds %struct.type_metadata_t, ptr %a_ptr, i64 2, i32 1, i32 0
+  %a4_metadata_ptr = getelementptr inbounds %struct.type_metadata_t, ptr %a_ptr, i64 3, i32 1, i32 0
+  %a5_metadata_ptr = getelementptr inbounds %struct.type_metadata_t, ptr %a_ptr, i64 4, i32 1, i32 0
+  %a6_metadata_count_ptr = getelementptr inbounds %struct.type_metadata_t, ptr %a_ptr, i64 5, i32 1, i32 0
+  %a6_metadata_types_ptr = getelementptr inbounds %struct.type_metadata_t, ptr %a_ptr, i64 5, i32 1, i32 0, i32 1
+  store i32 1, ptr %a1_ptr
+  store i32 5, ptr %a2_ptr
+  store i32 5, ptr %a3_ptr
+  store i32 3, ptr %a4_ptr
+  store i32 1, ptr %a5_ptr
+  store i32 6, ptr %a6_ptr
+  store i32 4, ptr %a1_metadata_ptr
+  store i32 4, ptr %a3_metadata_ptr
+  store i32 4, ptr %a4_metadata_ptr
+  store i32 4, ptr %a5_metadata_ptr
+  store i32 2, ptr %a6_metadata_count_ptr
+  store ptr %b_ptr, ptr %a6_metadata_types_ptr
+
+  call void @print_tuple(ptr @tuple, i32 6, ptr %a_ptr)
+  call void @printf(ptr @newline)
   ret i32 0
 }
-
-define void @createClosure(ptr sret(%closure_fn_type) %closure_fn_ptr, i64) {
-  entry:
-    %closure_fn = load %closure_fn_type, ptr %closure_fn_ptr
-    %closure_value_ptr = call ptr @malloc(i64 8)
-    %closure_value = load %closure_type, ptr %closure_value_ptr
-    %closure_value1 = insertvalue %closure_type %closure_value, i64 %0, 0
-    store %closure_type %closure_value1, ptr %closure_value_ptr
-    %closure_fn1 = insertvalue %closure_fn_type %closure_fn, ptr %closure_value_ptr, 1
-    %closure_fn2 = insertvalue %closure_fn_type %closure_fn1, ptr @closure, 0
-    store %closure_fn_type %closure_fn2, ptr %closure_fn_ptr
-    ret void
-}
-
-define i64 @closure(%closure_type, %fn_type) {
-entry:
-  %var_0 = extractvalue %closure_type %0, 0
-  %var_1 = call i64 %1(i64 %var_0)
-  ret i64 %var_1
-}
-
-define i64 @fn(i64) {
-entry:
-  ret i64 %0
-}
-
-declare i32 @printf(i8*, ...)
-declare ptr @malloc(i64)
